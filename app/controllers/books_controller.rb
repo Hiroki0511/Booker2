95b9b2e00eaf4dbdc0845fc84abf4dad26972332
githubアãHiroki0511 <ohtahiroki0511@gmail.com>
2020-07-06 15:17:11 +0000

    Initial Commit Message

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,56 @@
+class BooksController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+  	@books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	@book.user_id = current_user.id
+  	if @book.save
+  		 redirect_to book_path(@book.id), notice: "Book was successfully created."
+  	else
+      @user = current_user
+      @books = Book.all
+  	  render action: :index
+  	end
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+    #@user = @book.user
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+    else
+       redirect_to books_path
+    end
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  	   redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+       render action: :edit
+    end
+  end
+
+  def destroy
+  	@book = Book.find(params[:id])
+  	@book.destroy
+  	redirect_to books_path,notice: "Book was successfully destroyed."
+  end
+
+private
+
+  def book_params
+	  params.require(:book).permit(:title, :body, :user_id)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'cbcc4f6e3600aab73a54fdcbcefe5d29028f4d2b56851b762380cb139f91ff95d81442c714f870cf7f7e1d10a8ac3f1c30957dfb7d862ee0381b58c1f722d5e2'
